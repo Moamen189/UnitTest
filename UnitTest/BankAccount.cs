9fbb3625aa24e1c0d60e50fbe565341f98ae4b59
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-02-24 19:07:50 +0200

    Test

---
+++ UnitTest/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class BankAccount
+    {
+        public int Balance { get; private set; }
+
+        public BankAccount(int balance)
+        {
+            Balance = balance;
+        }
+        public void Depposit(int amount)
+        {
+        
+        }
+        public void WithDraw(int amount)
+        {
+           
+        }
+
+
+    }
+
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void BankAccountShouldIncreaseOnPositiveDeposit()
+        {
+
+        }
+    }
+}
