1782c08a4cd3c572da6dfba8e0066c61753b78bb
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-02-27 10:32:09 +0200

    Update DateDrivenTests

---
+++ UnitTest/BankAccountWithOverdraft.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTest
+{
+    internal class BankAccountWithOverdraft : BankAccount
+    {
+        private int startingBalance;
+        private int minBalance;
+
+        public BankAccountWithOverdraft(int startingBalance, int minBalance)
+        {
+            this.startingBalance = startingBalance;
+            this.minBalance = minBalance;
+        }
+    }
+}
